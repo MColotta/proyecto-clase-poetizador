8f4315a5d81bc47d4cb3d93a36c9e632521be69d
Matias C <mrcolotta@hotmail.com>
2018-04-24 20:00:04 -0300

    Cambios JS

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function customPoema() {
+  var x = document.getElementById("poema1");
+  var y = x.split(" ");
+}
+
+function cargarPoemaEnPantalla() {
+  document.getElementById("poema1").innerHTML = "";
+}
